e64165f51509d33e440055f02f5e77dc0b4e3aea
David Cabo <david.cabo@gmail.com>
2013-09-29 20:51:02 +0200

    Keep traceability of original URL for CNMV boards

--- cnmv-companies/parse_board.rb
+++ cnmv-companies/parse_board.rb
@@ -11,6 +11,10 @@ def parse_file(filename)
     # Some weird encoding issue going on CNMV site, not fully sure about this
     content = open(filename).read.force_encoding('UTF-8')
     
+    # Rebuild the source URL (can't do other way in a two step fetch/parse process)
+    filename =~ /staging\/(.*)_board/
+    via = "http://cnmv.es/Portal/Consultas/DerechosVoto/PS_AC_INI.aspx?nif=#{$1}"
+
     doc = Nokogiri::HTML(content)
     company_name = doc.css('#ctl00_lblSubtitulo').text
     board = doc.css('#ctl00_ContentPrincipal_gridConsejo')
@@ -22,10 +26,10 @@ def parse_file(filename)
         role = columns[3]
         shares = number_to_english(columns[1])
         from = columns[2]
-        puts CSV::generate_line([person_name, role, company_name, from, shares])
+        puts CSV::generate_line([person_name, role, company_name, from, shares, via])
     end
 end
 
-puts 'Source,Role,Company,From,Share %'
+puts 'Nombre,Cargo,Empresa,Fecha Nombramiento,% Derechos Voto,via'
 
 Dir['staging/*_board.html'].each {|filename| parse_file(filename)}
